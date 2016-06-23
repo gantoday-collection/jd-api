7b0363b4f46b44772174b5edb74c95d67cbdcb25
Gerald Chen <geraldchen@hellohippies.com>
2016-06-23 15:29:35 +0800

    update

--- tests/Jingdong/Tests/ProductTest.php
+++ tests/Jingdong/Tests/ProductTest.php
@@ -3,6 +3,7 @@ namespace Jingdong\Tests;
 
 use Jingdong\Helper\ProductHelper;
 
-$productHelper = new ProductHelper('0AAB224A4BED69377222BA60FA493AE8', '41cbe84513c842edb9ac1916f3092068');
-
-return var_dump($productHelper->search('iPhone'));
+class ProductTest extends \PHPUnit_Framework_TestCase
+{
+    //
+}
